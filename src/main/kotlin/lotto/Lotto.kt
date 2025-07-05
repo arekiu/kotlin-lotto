949117d7aa0b3f932d6265c36b5e76dbcb948605
arekiu <alexis.aguilera@deliveryhero.com>
2025-07-05 12:49:53 +0200

    refactor(Lotto): rename methods for better readability
    rename methods to show the purpose in a more clear way

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -3,19 +3,19 @@ package lotto
 class Lotto(val lottoNumbers: List<Int>) {
     init {
         require(lottoNumbers.count() == 6)
-        require(isInRange(lottoNumbers))
-        require(isDuplicated(lottoNumbers))
+        require(numbersAreInRange(lottoNumbers))
+        require(numbersAreNotDuplicated(lottoNumbers))
     }
 
     fun getNumbers(): List<Int> {
         return lottoNumbers
     }
 
-    private fun isInRange(lottoNumbers: List<Int>): Boolean {
+    private fun numbersAreInRange(lottoNumbers: List<Int>): Boolean {
         return lottoNumbers.all { it in 1..45 }
     }
 
-    private fun isDuplicated(lottoNumbers: List<Int>): Boolean {
+    private fun numbersAreNotDuplicated(lottoNumbers: List<Int>): Boolean {
         return lottoNumbers.count() == lottoNumbers.toSet().count()
     }
 }
