a1eec32bb3d34b07ac138882e8f5a1e192b1d746
arekiu <alexis.aguilera@deliveryhero.com>
2025-07-05 15:27:02 +0200

    refactor(Application): clean entry function
    redirect all the functionality to lottoController

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,22 +1,9 @@
 package lotto
 
+import lotto.controller.LottoController
+
 fun main() {
-    val inputView = InputView()
-    val lottoMachine = LottoMachine()
-    val resultView = ResultView()
+    val lottoController = LottoController()
 
-    // Create a ticket from given amount
-    val userAmount = inputView.takePurchaseInput()
-    val tickets = lottoMachine.createTickets(userAmount)
-    resultView.printLottos(tickets)
-
-    // Compare winning tickets from given winning numbers
-    val winningNumbers = inputView.takeWinningNumbers()
-    val bonusNumber = inputView.takeBonusNumber(winningNumbers)
-    val results = lottoMachine.compareTickets(tickets, winningNumbers, bonusNumber)
-    resultView.printResult(results)
-
-    // Calculate profit rate
-    val rate = lottoMachine.calculateReturnRate(results, userAmount)
-    resultView.printReturnRate(rate)
+    lottoController.runMachine()
 }
