473cb34880e6a3e111be3733afcdb7f931b2ff42
arekiu <alexis.aguilera@deliveryhero.com>
2025-07-06 17:49:01 +0200

    refactor(TicketSeller): remove property from constructor

--- src/main/kotlin/lotto/model/TicketsSeller.kt
+++ src/main/kotlin/lotto/model/TicketsSeller.kt
@@ -1,14 +1,13 @@
 package lotto.model
 
 class TicketsSeller(
-    private val userAmount: Int,
     private val ticketGenerator: TicketGenerator = TicketGenerator(),
     private val numberGenerator: NumberGenerator = NumberGenerator()
 ) {
 
     fun calculateTickets(userAmount: Int) = userAmount / LOTTO_PRICE
 
-    fun createRandomTickets(): List<Lotto> {
+    fun createRandomTickets(userAmount: Int): List<Lotto> {
         val amountOfTickets = calculateTickets(userAmount)
         return List(amountOfTickets) { ticketGenerator.createLottoTicket(numberGenerator.generateNumbers()) }
     }
--- src/test/kotlin/TicketSellerTest.kt
+++ src/test/kotlin/TicketSellerTest.kt
@@ -7,15 +7,15 @@ import org.junit.jupiter.api.Assertions.assertEquals
 class TicketSellerTest {
     @Test
     fun `should return right amount of tickets`() {
-        val ticketSeller = TicketsSeller(5000)
-        val tickets = ticketSeller.createRandomTickets()
+        val ticketSeller = TicketsSeller()
+        val tickets = ticketSeller.createRandomTickets(5000)
         assertEquals(tickets.count(), 5)
     }
 
     @Test
     fun `ticket should contain 6 numbers`() {
-        val ticketSeller = TicketsSeller(1000)
-        val tickets = ticketSeller.createRandomTickets()
+        val ticketSeller = TicketsSeller()
+        val tickets = ticketSeller.createRandomTickets(1000)
         val numbersInTicket = tickets[0].lottoNumbers
         assertEquals(numbersInTicket.count(), 6)
     }
