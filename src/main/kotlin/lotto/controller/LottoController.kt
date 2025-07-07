7e9e4be1c1583e45467525cd4391dcacfd1fd833
arekiu <alexis.aguilera@deliveryhero.com>
2025-07-07 14:35:34 +0200

    refactor(LottoController): rearrange class to include refactored methods

--- src/main/kotlin/lotto/controller/LottoController.kt
+++ src/main/kotlin/lotto/controller/LottoController.kt
@@ -1,8 +1,6 @@
 package lotto.controller
 
-import lotto.model.LottoMachine
-import lotto.model.PrizeNumbers
-import lotto.model.TicketsSeller
+import lotto.model.*
 import lotto.view.InputView
 import lotto.view.ResultView
 
@@ -10,20 +8,27 @@ class LottoController {
     private val inputView = InputView()
     private val resultView = ResultView()
     private val ticketSeller = TicketsSeller()
+    private val ticketGenerator = TicketGenerator()
 
     fun run() {
         val userAmount = inputView.takePurchaseInput()
         val tickets = ticketSeller.createRandomTickets(userAmount)
         resultView.printLottos(tickets)
 
-        val winningNumbers = inputView.takeWinningNumbers()
-        val bonusNumber = inputView.takeBonusNumber(winningNumbers)
-        val prizeNumbers = PrizeNumbers(winningNumbers, bonusNumber)
+        val prizeNumbers = createPrizeNumbers()
         val dataLottoMachine = LottoMachine(tickets, prizeNumbers)
         handleResults(dataLottoMachine, userAmount)
     }
 
-    fun handleResults(lottoMachine : LottoMachine, userAmount: Int) {
+    fun createPrizeNumbers(): PrizeNumbers {
+        val winningNumbers = inputView.takeWinningNumbers()
+        val bonusNumber = inputView.takeBonusNumber(winningNumbers)
+        val winningLottoNumbers = ticketGenerator.generateLottoNumbers(winningNumbers)
+        val bonusLottoNumber = LottoNumber.from(bonusNumber)
+        return PrizeNumbers(winningLottoNumbers, bonusLottoNumber)
+    }
+
+    fun handleResults(lottoMachine: LottoMachine, userAmount: Int) {
         val ticketsResult = lottoMachine.evaluateTicketsResult()
         val returnRate = lottoMachine.evaluateReturnRate(ticketsResult, userAmount)
         resultView.printResult(ticketsResult, returnRate)
