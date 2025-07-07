4a85f4f276a1d9469558d86c977978473e9218f0
arekiu <alexis.aguilera@deliveryhero.com>
2025-07-07 14:21:36 +0200

    refactor(LottoNumber): add dedicated class for Lotto numbers

---
+++ src/main/kotlin/lotto/model/LottoNumber.kt
@@ -0,0 +1,17 @@
+package lotto.model
+
+class LottoNumber private constructor(private val value: Int) {
+
+    companion object {
+        private const val MINIMUM_NUMBER = 1
+        private const val MAXIMUM_NUMBER = 45
+        private val NUMBERS: Map<Int, LottoNumber> =
+            (MINIMUM_NUMBER..MAXIMUM_NUMBER).associateWith(::LottoNumber)
+
+        fun from(value: Int): LottoNumber {
+            return NUMBERS[value] ?: throw IllegalArgumentException()
+        }
+    }
+
+    override fun toString(): String = value.toString()
+}
