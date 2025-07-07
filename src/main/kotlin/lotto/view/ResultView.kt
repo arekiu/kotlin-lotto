94e4432704f19ed94158333eecade8b615601905
arekiu <alexis.aguilera@deliveryhero.com>
2025-07-07 14:34:40 +0200

    refactor(ResultView): modify printing method
    replace type for printing lotto Numbers

--- src/main/kotlin/lotto/view/ResultView.kt
+++ src/main/kotlin/lotto/view/ResultView.kt
@@ -1,14 +1,13 @@
 package lotto.view
 
-import lotto.model.Lotto
+import lotto.model.LottoTicket
 import lotto.model.Rank
 
 class ResultView() {
-    fun printLottos(lottos: List<Lotto>) {
+    fun printLottos(lottos: List<LottoTicket>) {
         println("You have purchased ${lottos.count()} tickets.")
         lottos.forEach { lotto ->
-            val lottoNumbers = lotto.lottoNumbers
-            println(lottoNumbers.joinToString(",", "[", "]"))
+            println(lotto.lottoNumbers.joinToString(",", "[", "]"))
         }
     }
 
@@ -18,16 +17,16 @@ class ResultView() {
         printReturnRate(returnRate)
     }
 
-    fun printResultTickets(results: MutableMap<Rank, Int>){
+    fun printResultTickets(results: MutableMap<Rank, Int>) {
         println(
             """
         $PRESENT_WINNING
         $SEPARATOR
-        3 Matches (${ "%,d".format(Rank.FIFTH.winningMoney) } KRW) - ${results.getValue(Rank.FIFTH)} tickets
-        4 Matches (${ "%,d".format(Rank.FOURTH.winningMoney) } KRW) - ${results.getValue(Rank.FOURTH)} tickets
-        5 Matches (${ "%,d".format(Rank.THIRD.winningMoney) } KRW) - ${results.getValue(Rank.THIRD)} tickets
-        5 Matches + Bonus Ball (${ "%,d".format(Rank.SECOND.winningMoney) } KRW) - ${results.getValue(Rank.SECOND)} tickets
-        6 Matches (${ "%,d".format(Rank.FIRST.winningMoney) } KRW) - ${results.getValue(Rank.FIRST)} tickets
+        3 Matches (${"%,d".format(Rank.FIFTH.winningMoney)} KRW) - ${results.getValue(Rank.FIFTH)} tickets
+        4 Matches (${"%,d".format(Rank.FOURTH.winningMoney)} KRW) - ${results.getValue(Rank.FOURTH)} tickets
+        5 Matches (${"%,d".format(Rank.THIRD.winningMoney)} KRW) - ${results.getValue(Rank.THIRD)} tickets
+        5 Matches + Bonus Ball (${"%,d".format(Rank.SECOND.winningMoney)} KRW) - ${results.getValue(Rank.SECOND)} tickets
+        6 Matches (${"%,d".format(Rank.FIRST.winningMoney)} KRW) - ${results.getValue(Rank.FIRST)} tickets
         """.trimIndent()
         )
     }
