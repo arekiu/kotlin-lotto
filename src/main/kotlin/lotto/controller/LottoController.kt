d1b8e40038c6d0d5e61944c43ef0f6c56d5098df
arekiu <alexis.aguilera@deliveryhero.com>
2025-07-07 10:49:00 +0200

    refactor(LottoController): remove userAmount field
    remove field and create local variable to store the user amount of money

--- src/main/kotlin/lotto/controller/LottoController.kt
+++ src/main/kotlin/lotto/controller/LottoController.kt
@@ -11,29 +11,21 @@ class LottoController {
     private val resultView = ResultView()
     private val ticketSeller = TicketsSeller()
 
-    private var userAmount = 0
-
-    fun runMachine() {
-        val dataLottoMachine = handleUserInput()
-        handleResults(dataLottoMachine)
-    }
-
-    fun handleUserInput(): LottoMachine {
-        userAmount = inputView.takePurchaseInput()
+    fun run() {
+        val userAmount = inputView.takePurchaseInput()
         val tickets = ticketSeller.createRandomTickets(userAmount)
         resultView.printLottos(tickets)
 
         val winningNumbers = inputView.takeWinningNumbers()
         val bonusNumber = inputView.takeBonusNumber(winningNumbers)
         val prizeNumbers = PrizeNumbers(winningNumbers, bonusNumber)
-        return LottoMachine(tickets, prizeNumbers)
+        val dataLottoMachine = LottoMachine(tickets, prizeNumbers)
+        handleResults(dataLottoMachine, userAmount)
     }
 
-    fun handleResults(lottoMachine : LottoMachine) {
+    fun handleResults(lottoMachine : LottoMachine, userAmount: Int) {
         val ticketsResult = lottoMachine.evaluateTicketsResult()
-        resultView.printResult(ticketsResult)
-
         val returnRate = lottoMachine.evaluateReturnRate(ticketsResult, userAmount)
-        resultView.printReturnRate(returnRate)
+        resultView.printResult(ticketsResult, returnRate)
     }
 }
