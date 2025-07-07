d7d523195b3136bcbf3eec3b70b24bf92446ec41
arekiu <alexis.aguilera@deliveryhero.com>
2025-07-07 14:23:55 +0200

    refactor(NumberGenerator): rename method
    change name from generateNumbers() to generateRandomNumbers() for more clarity

--- src/main/kotlin/lotto/model/NumberGenerator.kt
+++ src/main/kotlin/lotto/model/NumberGenerator.kt
@@ -1,10 +1,8 @@
 package lotto.model
 
-import kotlin.collections.shuffled
-
 class NumberGenerator {
 
-    fun generateNumbers() = (MINIMUM_VALUE..MAXIMUM_VALUE).shuffled().take(6).sorted()
+    fun generateRandomNumbers(): List<Int> = (MINIMUM_VALUE..MAXIMUM_VALUE).shuffled().take(6).sorted()
 
     companion object {
         const val MINIMUM_VALUE = 1
