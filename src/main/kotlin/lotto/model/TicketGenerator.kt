9808e9708e1cefc788421f093a73928a13be7beb
arekiu <alexis.aguilera@deliveryhero.com>
2025-07-08 21:38:26 +0200

    refactor(TicketGenerator): make generateLottoNumbers() private

--- src/main/kotlin/lotto/model/TicketGenerator.kt
+++ src/main/kotlin/lotto/model/TicketGenerator.kt
@@ -2,7 +2,7 @@ package lotto.model
 
 class TicketGenerator() {
 
-    fun generateLottoNumbers(numbers: List<Int>) = numbers.map { number -> LottoNumber.from(number) }
+    private fun generateLottoNumbers(numbers: List<Int>) = numbers.map { number -> LottoNumber.from(number) }
 
     fun createLottoTicket(numbers: List<Int>): LottoTicket {
         return LottoTicket(generateLottoNumbers(numbers))
