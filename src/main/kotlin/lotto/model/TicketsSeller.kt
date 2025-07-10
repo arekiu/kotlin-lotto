8f45272479a97f8066faf86cad3d0dfcdc0a07b9
arekiu <alexis.aguilera@deliveryhero.com>
2025-07-10 09:06:59 +0200

    refactor(NumbersGenerator): create interface for numbers generator

--- src/main/kotlin/lotto/model/TicketsSeller.kt
+++ src/main/kotlin/lotto/model/TicketsSeller.kt
@@ -1,17 +1,15 @@
 package lotto.model
 
 class TicketsSeller(
-    private val numberGenerator: NumberGenerator = NumberGenerator()
+    private val numbersGenerator: NumbersGenerator = RandomNumbersGenerator(),
 ) {
-
     fun calculateTickets(userAmount: Int) = userAmount / LOTTO_PRICE
 
     fun createRandomTickets(numberOfTickets: Int): List<LottoTicket> {
-        return List(numberOfTickets) { LottoTicket(numberGenerator.generateRandomNumbers()) }
+        return List(numberOfTickets) { LottoTicket(numbersGenerator.generateNumbers()) }
     }
 
-    fun createCustomTickets(numbersForLottos: List<List<Int>>) =
-        numbersForLottos.map { LottoTicket(it) }
+    fun createCustomTickets(numbersForLottos: List<List<Int>>) = numbersForLottos.map { LottoTicket(it) }
 
     companion object {
         const val LOTTO_PRICE = 1000
